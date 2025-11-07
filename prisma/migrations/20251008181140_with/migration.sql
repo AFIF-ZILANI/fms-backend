591691dd1b6bd7dd883e2810baebda2f713e3d8d
AFIF ZILANI <afifzilani4566@gmail.com>
2025-11-08 00:51:46 +0600

    refactor: remove unused migration files and database functions

--- prisma/migrations/20251008181140_with/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "Medications" ADD CONSTRAINT "Medications_administered_by_fkey" FOREIGN KEY ("administered_by") REFERENCES "Admin"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
